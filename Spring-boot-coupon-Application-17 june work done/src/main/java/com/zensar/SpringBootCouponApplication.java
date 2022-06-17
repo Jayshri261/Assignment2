094ef7b6593c7a0cf61f980b2bf2e193ab471c32
Jayshri261 <105355446+Jayshri261@users.noreply.github.com>
2022-06-17 22:30:02 +0530

    Add files via upload

---
+++ Spring-boot-coupon-Application-17 june work done/src/main/java/com/zensar/SpringBootCouponApplication.java
@@ -0,0 +1,32 @@
+package com.zensar;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = { "com.zensar" })
+public class SpringBootCouponApplication {
+	// public class SpringBootProjectApplication extends
+	// SpringBootServletInitializer
+
+	public static void main(String[] args) {
+		System.out.println("Hii Jayshri");
+		SpringApplication.run(SpringBootCouponApplication.class, args);
+		System.out.println("Application Started");
+	}
+
+	// @Override
+	// protected SpringApplicationBuilder configure(SpringApplicationBuilder
+	// builder) {
+	// TODO Auto-generated method stub
+	// return super.configure(builder);
+	// }
+	@Bean
+	public ModelMapper getModelMapper() {
+		return new ModelMapper();
+	}
+
+}
